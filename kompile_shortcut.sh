8028688edeab6e53a9f7e6608c269a9ab228c24a
Andrew Capon <andrew.capon@ocubis.co.uk>
2024-11-18 08:22:25 +0000

    Alternative developer build setup for using msys bash

--- kompile_shortcut.sh
+++ kompile_shortcut.sh
@@ -16,6 +16,10 @@ case "$unamestr" in
         platform='windows'
         rootdir="$(cd $(dirname $0); pwd -P)"
     ;;
+    MSYS*)
+        platform='windows_bash'
+        rootdir="$(cd $(dirname $0); pwd -P)"
+    ;;
     *)
         echo "Unknown OS: $unamestr - aborting..."
         exit
@@ -52,7 +56,7 @@ case "$platform" in
         # sh ./qlean.sh
 
         # compile board mode and firmware options
-        sh ./platform_linux/compile_firmware.sh BOARD_AXOLOTI_CORE 2>&1 | tee firmware.logB
+        sh ./platform_linux/compile_firmware.sh BOARD_AXOLOTI_CORE 2>&1 | tee firmware.log
         # create .lst files
         ./platform_linux/bin/arm-none-eabi-objdump --source-comment --demangle --disassemble ./firmware/flasher/flasher_build/axoloti_flasher/axoloti_flasher.elf > ./firmware/flasher/flasher_build/axoloti_flasher.lst
         ./platform_linux/bin/arm-none-eabi-objdump --source-comment --demangle --disassemble ./firmware/mounter/mounter_build/axoloti_mounter/axoloti_mounter.elf > ./firmware/mounter/mounter_build/axoloti_mounter.lst 
@@ -93,5 +97,28 @@ case "$platform" in
 
         cd ..
         sh ./qlean.sh
+    ;;
+    windows_bash)
+        # rm -f ./firmware/build/*.*
+        # sh ./qlean.sh
+
+        # compile board mode and firmware options
+        sh ./platform_win/compile_firmware.sh BOARD_AXOLOTI_CORE 2>&1 | tee firmware.logB
+        # create .lst files
+        ./platform_win/bin/arm-none-eabi-objdump.exe --source-comment --demangle --disassemble ./firmware/flasher/flasher_build/axoloti_flasher/axoloti_flasher.elf > ./firmware/flasher/flasher_build/axoloti_flasher.lst
+        ./platform_win/bin/arm-none-eabi-objdump.exe --source-comment --demangle --disassemble ./firmware/mounter/mounter_build/axoloti_mounter/axoloti_mounter.elf > ./firmware/mounter/mounter_build/axoloti_mounter.lst 
+        ./platform_win/bin/arm-none-eabi-objdump.exe --source-comment --demangle --disassemble ./firmware/build/axoloti/normal/axoloti.elf > ./firmware/build/axoloti.lst
+        ./platform_win/bin/arm-none-eabi-objdump.exe --source-comment --demangle --disassemble ./firmware/build/axoloti/spilink/axoloti_spilink.elf > ./firmware/build/axoloti_spilink.lst
+        ./platform_win/bin/arm-none-eabi-objdump.exe --source-comment --demangle --disassemble ./firmware/build/axoloti/usbaudio/axoloti_usbaudio.elf > ./firmware/build/axoloti_usbaudio.lst
+
+        # compile board mode and firmware options
+        sh ./platform_win/compile_firmware.sh BOARD_KSOLOTI_CORE 2>&1 | tee -a firmware.log
+        # create .lst files
+        ./platform_win/bin/arm-none-eabi-objdump.exe --source-comment --demangle --disassemble ./firmware/flasher/flasher_build/ksoloti_flasher/ksoloti_flasher.elf > ./firmware/flasher/flasher_build/ksoloti_flasher.lst
+        ./platform_win/bin/arm-none-eabi-objdump.exe --source-comment --demangle --disassemble ./firmware/mounter/mounter_build/ksoloti_mounter/ksoloti_mounter.elf > ./firmware/mounter/mounter_build/ksoloti_mounter.lst
+        ./platform_win/bin/arm-none-eabi-objdump.exe --source-comment --demangle --disassemble ./firmware/build/ksoloti/normal/ksoloti.elf > ./firmware/build/ksoloti.lst
+        ./platform_win/bin/arm-none-eabi-objdump.exe --source-comment --demangle --disassemble ./firmware/build/ksoloti/spilink/ksoloti_spilink.elf > ./firmware/build/ksoloti_spilink.lst
+        ./platform_win/bin/arm-none-eabi-objdump.exe --source-comment --demangle --disassemble ./firmware/build/ksoloti/usbaudio/ksoloti_usbaudio.elf > ./firmware/build/ksoloti_usbaudio.lst
+        # sh ./qlean.sh
     ;;
 esac
